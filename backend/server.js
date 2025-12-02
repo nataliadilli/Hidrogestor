bdd00e70ba42cb9c4820dbe32bade556417161fd
nataliadilli <128419482+nataliadilli@users.noreply.github.com>
2025-12-02 20:24:33 -0300

    Ajuste server.

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,10 @@ const db = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.query("SELECT 1")
